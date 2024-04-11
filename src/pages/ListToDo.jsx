03fca727792c9bf7b5f67c01a21f4c762448db12
Citronskal <yiaswift@gmail.com>
2024-04-11 20:40:22 +0200

    Create more components

--- src/pages/ListToDo.jsx
+++ src/pages/ListToDo.jsx
@@ -1,15 +1,32 @@
 import { useToDos } from "../ToDoContext";
 
 export const ListToDo = () => {
-
-  const 
+  const { todos, toggleToDo } = useToDos();
 
   return (
     <div>
       <h1>All</h1>
-      <p>show number of tasks</p>
-      <div>pass down created tasks</div>
-      <button>+</button>
+      <p>
+        {todos.length} {todos.length === 1 ? "task" : "tasks"}
+      </p>
+      <div>
+        {todos.map((todo, index) => (
+          <div key={index}>
+            <input
+              type="checkbox"
+              checked={todo.completed}
+              onChange={() => {
+                toggleToDo(index);
+              }}
+            />
+            <label
+              className={`${todo.completed} ? "text-gray-400 line-through" : "text-black"`}
+            >
+              {todo.text}
+            </label>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
